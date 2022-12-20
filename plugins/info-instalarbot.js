dc7a3fe4a3d22495a89b9e1b009ae110b582c7f3
Ⓢ︎Ⓝ︎Ⓣ︎ <120823387+SNT-Oficial@users.noreply.github.com>
2022-12-19 21:14:41 -0300

    Update info-instalarbot.js

--- plugins/info-instalarbot.js
+++ plugins/info-instalarbot.js
@@ -1,19 +1,6 @@
 import { generateWAMessageFromContent } from '@adiwajshing/baileys'
 let handler  = async (m, { conn }) => {
 let texto = `
-*—◉ 𝚃𝚄𝚃𝙾𝚁𝙸𝙰𝙻 𝙱𝙾𝚇𝙼𝙸𝙽𝙴 𝙷𝙾𝚂𝚃*
-> Tutorial: https://youtu.be/eC9TfKICpcY
-> Pagina Oficial: https://boxmineworld.com
-> Dashboard: https://dash.boxmineworld.com/home
-> Panel: https://panel.boxmineworld.com
-> Soporte: https://discord.gg/84qsr4v 
-
-------------------------------------
-
-*—◉ 𝚃𝚄𝚃𝙾𝚁𝙸𝙰𝙻 𝙰𝙲𝙸𝙳𝙸𝙲𝙽𝙾𝙳𝙴𝚂 𝙷𝙾𝚂𝚃*
-> Tutorial: https://youtu.be/nbjvreJ0tUk
-> Pagina: https://billing.acidicnodes.ml/register?ref=ADII104p
-> Soporte: https://whatsapp.acidicnodes.com
 
 ------------------------------------
 
@@ -21,7 +8,7 @@ let texto = `
 > cd && termux-setup-storage
 > apt-get update -y && apt-get upgrade -y
 > pkg install -y git nodejs ffmpeg imagemagick && pkg install yarn 
-> git clone https://github.com/BrunoSobrino/Mystic-termux.git && cd Mystic-termux 
+> git clone https://github.com/SNT-Oficial/SapitoV3.git && cd SapitoV3
 > yarn install 
 > npm install
 > npm update
