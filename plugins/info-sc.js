887c7ff97c85d1a271713afd22402c3db6fe8e2a
Ⓢ︎Ⓝ︎Ⓣ︎ <120823387+SNT-Oficial@users.noreply.github.com>
2022-12-19 21:15:43 -0300

    Update info-sc.js

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, usedPrefix }) => {
 var doc = ['pdf','zip','vnd.openxmlformats-officedocument.presentationml.presentation','vnd.openxmlformats-officedocument.spreadsheetml.sheet','vnd.openxmlformats-officedocument.wordprocessingml.document']
 var document = doc[Math.floor(Math.random() * doc.length)]    
-let texto1 = `*https://github.com/BrunoSobrino/TheMystic-Bot-MD*`
+let texto1 = `*https://github.com/SNT-Oficial/SapitoV4*`
 let buttonMessage= {
 'document': { url: `https://github.com/BrunoSobrino/TheMystic-Bot-MD` },
 'mimetype': `application/${document}`,
